e9b257660b8ed611435c9ebe47328af1e79639a8
joseavc97 <joseavillalobos1997@gmail.com>
2022-05-22 19:06:31 -0500

    Popular page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Pages from './pages/Pages';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Popular.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+
+function Popular() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+
+  return (
+    <div>
+      {popular.map((recipe) => (
+        <div key={recipe.id}>
+          <p>{recipe.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Popular;
