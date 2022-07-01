5188f20d5bcf893f7928d9b3df8216eab02dc3bf
joseavc <joseavillalobos1997@gmail.com>
2022-07-01 16:11:56 -0500

    NavLink component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages/Pages';
+import Category from './components/Category';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
