51d9b734d779f9270717133f796f628a3c7317ec
joseavc <joseavillalobos1997@gmail.com>
2022-07-01 20:24:31 -0500

    Search bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages/Pages';
 import Category from './components/Category';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Cuisine from './Cuisine';
+import Searched from './Searched';
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
     </Routes>
   );
 }
